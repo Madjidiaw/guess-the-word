5cf331cdd12fff619583d11b9b55810791ce07c8
madji <mdiaw13@gmail.com>
2021-10-05 16:54:28 -0400

    Setup needed HTML elements and the placeholders

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Unordered list to hold a player's guessed letter
+const listGuessedLetters = document.querySelector(".guessed-letters");
+// Button with the guess text
+const guessButton = document.querySelector(".guess");
+// Text input where player will guess a letter
+const textInput  = document.querySelector(".letter");
+// Empty paragraph where the word in progress will appear
+const wordInProgress = document.querySelector(".word-in-progress");
+// Paragraph to display remaining guesses
+const remainingGuess = document.querySelector(".remaining");
+// Span inside the paragraph to display remaining guesses
+const guessSpanInside = document.querySelector("span");
+// Message holder when player guess a letter 
+const messageHolder = document.querySelector(".message");
+// Prompt player to try again
+const playAgainButton = document.querySelector(".play-again");
+// Starting word to test and initial response from the API
+const word = "magnolia";
+
+// Add placeholders for each letter
+const updateWordInProgress = function () {
+   wordInProgress.innerText = "●"; 
+};
+
+updateWordInProgress(word);
+
+// Add event listener for guess button
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const enteredGuess = e.target.value;
+    //enteredGuess = "";
+});
